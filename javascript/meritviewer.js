41d55577a93e07d0ec4a8967d95ce7fa5415fecb
stevenjburns@msn.com <stevenjburns@msn.com>
2018-02-04 06:46:26 -0700

    Theme color changes based on btn clicks; selected and hover empire displays correctly

--- javascript/meritviewer.js
+++ javascript/meritviewer.js
@@ -1,14 +1,25 @@
 "use strict";
 
 let AppData = {};
+let currentEmpire = "";
 const dataURL = "../data/meritviewer-data.json";
 
 const empireColors = {
-  "all" : ["black", "silver"],
-  "nc" : ["navy", "gold"],
-  "tr" : ["darkred", "black"],
-  "vs" : ["darkpurple", "seagreen"]
-}
+  "all" : ["grey darken-4", "silver"],
+  "nc" : ["indigo darken-4", "gold"],
+  "tr" : ["red darken-4", "black"],
+  "vs" : ["purple darken-4", "seagreen"]
+};
+
+$('img').hover(
+  () => {
+    let lbl = $(event.target).data('empire-label');
+    $('#h5-hover-empire').text(lbl);
+  },
+  () => {
+    $('#h5-hover-empire').text("")
+  }
+);
 
 const fetchAppData = (url) => {
   fetch(url)
@@ -16,8 +27,10 @@ const fetchAppData = (url) => {
     .then(res => AppData = res)
 };
 
-const changeEmpireGradientTheme = (empreColorArray) => {
-  console.log("From the changeEmpireGradientTheme function");
+const changeSelectedEmpire = (newEmpire) => {
+  currentEmpire = newEmpire;
+  $('header, footer').attr("class", empireColors[newEmpire][0]);
+  $('#h4-selected-empire').text($(event.target).data('empire-label'));
 }
 
 fetchAppData(dataURL);
